0cf556f5b8961a0639d4597e052c986aef5a41e1
Viktor Shterev <viktor.shterev55@gmail.com>
2025-02-28 08:34:56 +0200

    first touch with comparator and iterator

---
+++ IteratorsAndComparators/Lab/Book/BookComparator.java
@@ -0,0 +1,17 @@
+package IteratorsAndComparators.Lab.Book;
+
+import java.util.Comparator;
+
+public class BookComparator implements Comparator<Book> {
+
+    @Override
+    public int compare(Book f, Book s) {
+
+        int compared = f.getTitle().compareTo(s.getTitle());
+
+        if (compared == 0) {
+            compared = Integer.compare(f.getYear(), s.getYear());
+        }
+        return compared;
+    }
+}
